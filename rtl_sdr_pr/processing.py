e69e6292ded5a2ceac6078d3c4a2dae2dc3950a7
Andreas Baulig <free.geronimo@hotmail.de>
2021-05-11 22:08:00 +0200

    Implement fast CAF based on DFT of Lag Product

--- rtl_sdr_pr/processing.py
+++ rtl_sdr_pr/processing.py
@@ -1,6 +1,9 @@
 import numpy as np
 import numpy.typing as npt
 
+import scipy.signal
+import scipy.fft
+
 
 def direct_ambiguity(
     delay_bins: npt.ArrayLike,
@@ -57,6 +60,76 @@ def direct_ambiguity(
     lag_prod = reference_signal[n] * echo_signal[n_lag].conjugate()
     dop_exp = np.exp(-2j * np.pi * np.outer(n, doppler_bins) / sampling_freq)
     return np.matmul(lag_prod, dop_exp)
+
+
+def fast_ambiguity(
+    num_delay_bins: int,
+    num_doppler_bins: int,
+    reference_signal: np.ndarray,
+    surveillance_signal: np.ndarray,
+) -> np.ndarray:
+
+    """Fast implementation of cross ambiguity function (CAF), using Fourier
+    Transform of Lag Product approach.
+
+    Parameters
+    ----------
+    num_delay_bins : int
+        Number of delay bins by which to shift the surveillance signal in
+        time-domain and calculate the CAF. Can be scalar or array-like. Must be
+        `>= 1`.
+    num_doppler_bins : int
+        Number of frequency bins by which to shift the surveillance signal in
+        frequency-domain and calculate the CAF. Can be scalar or array-like.
+        Must be `>= 1`.
+    reference_signal : np.ndarray
+        Samples from the surveillance channel, which contains the direct path
+        signal.
+    surveillance_signal : np.ndarray
+        Samples from the surveillance channel, which contains target echos.
+
+    Returns
+    -------
+    np.ndarray
+        Cross ambiguity of surveillance and reference signal as 2D array with
+        size `num_delay_bins` x `num_doppler_bins`.
+
+    Examples
+    --------
+    Calculate the CAF of a real-valued rectangular pulse shifted by 50 samples.
+    This will produce a triangular CAF with its peak at the 50th element.
+    >>> reference = np.pad(np.ones(10), (0, 1000))
+    >>> surveillance = np.roll(reference, 50)
+    >>> amb = fast_ambiguity(
+    ...     num_delay_bins=100,
+    ...     num_doppler_bins=1,
+    ...     reference_signal=reference,
+    ...     surveillance_signal=surveillance)
+    >>> np.argmax(amb)
+    50
+    """
+
+    num_taps = reference_signal.shape[0] // num_doppler_bins
+
+    fir = scipy.signal.dlti(np.ones(num_taps), 1)
+
+    amb = np.empty((num_delay_bins, num_doppler_bins), dtype=np.complex64)
+
+    surv_cmplx_conj = surveillance_signal.conjugate()
+    surv_cmplx_conj = np.pad(surv_cmplx_conj, pad_width=(0, num_delay_bins))
+
+    for delay_bin, lag in enumerate(-np.arange(num_delay_bins)):
+        lag_product = (
+            reference_signal
+            * np.roll(surv_cmplx_conj, lag)[: reference_signal.shape[0]]
+        )
+        amb[delay_bin, :] = scipy.signal.decimate(
+            lag_product, num_taps, ftype=fir
+        )[:num_doppler_bins]
+
+    amb = scipy.fft.fftshift(scipy.fft.fft(amb, axis=1), axes=1)
+
+    return amb
 
 
 if __name__ == "__main__":
