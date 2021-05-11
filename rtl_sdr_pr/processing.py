26b28c06cb6c12378c6e58392695a54e1bcba4fc
Andreas Baulig <free.geronimo@hotmail.de>
2021-05-11 22:09:31 +0200

    Change parameter name to surveillance_signal

--- rtl_sdr_pr/processing.py
+++ rtl_sdr_pr/processing.py
@@ -1,14 +1,14 @@
 import numpy as np
-import numpy.typing as npt
+from numpy.typing import ArrayLike
 
 import scipy.signal
 import scipy.fft
 
 
 def direct_ambiguity(
-    delay_bins: npt.ArrayLike,
-    doppler_bins: npt.ArrayLike,
-    echo_signal: np.ndarray,
+    delay_bins: ArrayLike,
+    doppler_bins: ArrayLike,
+    surveillance_signal: np.ndarray,
     reference_signal: np.ndarray,
     sampling_freq: float,
 ) -> np.ndarray:
@@ -16,34 +16,36 @@ def direct_ambiguity(
 
     Parameters
     ----------
-    delay_bins : npt.ArrayLike
-        Delay bin(s) by which to shift the echo signal in time-domain and
-        calculate the CAF. Can be scalar or array-like.
-    doppler_bins : npt.ArrayLike
-        Frequency bin(s) by which to shift the echo signal in frequency-domain
+    delay_bins : ArrayLike
+        Delay bin(s) by which to shift the surveillance signal in time-domain
         and calculate the CAF. Can be scalar or array-like.
-    echo_signal : np.ndarray
-        Samples representing the echo signal received via the indirect paths.
+    doppler_bins : ArrayLike
+        Frequency bin(s) by which to shift the surveillance signal in
+        frequency-domain and calculate the CAF. Can be scalar or array-like.
     reference_signal : np.ndarray
-        Samples representing the reference signal received via the direct path.
+        Samples from the surveillance channel, which contains the direct path
+        signal.
+    surveillance_signal : np.ndarray
+        Samples from the surveillance channel, which contains target echos.
     sampling_freq : float
-        Sampling frequency of both echo and reference samples.
+        Sampling frequency of both surveillance and reference samples.
 
     Returns
     -------
     np.ndarray
-        Cross ambiguity of echo and reference signal.
+        Cross ambiguity of surveillance and reference signal as 2D array with
+        size `num_delay_bins` x `num_doppler_bins`.
 
     Examples
     --------
     Calculate the CAF of a real-valued rectangular pulse shifted by 50 samples.
     This will produce a triangular CAF with its peak at the 50th element.
     >>> reference = np.pad(np.ones(10), (0, 1000))
-    >>> echo = np.roll(reference, 50)
+    >>> surveillance = np.roll(reference, 50)
     >>> amb = direct_ambiguity(
     ...     delay_bins=range(100),
     ...     doppler_bins=0,
-    ...     echo_signal=echo,
+    ...     surveillance_signal=surveillance,
     ...     reference_signal=reference,
     ...     sampling_freq=1e3)
     >>> np.argmax(amb)
@@ -51,13 +53,13 @@ def direct_ambiguity(
     """
     delay_bins = np.asarray(delay_bins)
     doppler_bins = np.asarray(doppler_bins)
-    num_samples = echo_signal.shape[0]
+    num_samples = surveillance_signal.shape[0]
 
-    echo_signal = np.pad(echo_signal, (0, num_samples))
+    surveillance_signal = np.pad(surveillance_signal, (0, num_samples))
 
     n = np.arange(num_samples)
     n_lag = np.int32(np.outer(delay_bins, np.ones((num_samples))) + n)
-    lag_prod = reference_signal[n] * echo_signal[n_lag].conjugate()
+    lag_prod = reference_signal[n] * surveillance_signal[n_lag].conjugate()
     dop_exp = np.exp(-2j * np.pi * np.outer(n, doppler_bins) / sampling_freq)
     return np.matmul(lag_prod, dop_exp)
 
