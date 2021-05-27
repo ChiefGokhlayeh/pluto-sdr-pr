200d80b86ad624e24050200b7093206167b60a6b
Braafisch <w.bradfisch@gmx.de>
2021-05-27 21:38:46 +0200

    Implement sdriq parser

--- rtl_sdr_pr/ioutils.py
+++ rtl_sdr_pr/ioutils.py
@@ -29,6 +29,46 @@ def read_samples(
     return n
 
 
+def read_sdriq_samples(
+    fid: typing.BinaryIO, num_samples: int, offset: typing.Optional[int] = 0
+):
+    """
+    Read specified number of samples from sdriq file.
+
+    :param fid: IO-like from which to read.
+    :param num_samples: Number of samples to read.
+    :param offset: Offset (in samples) to skip before reading.
+    :returns An array-like with the given numer of samples or less if EOF
+    reached.
+    """
+
+    header = np.fromfile(fid, dtype=np.uint8, count=32)
+
+    (
+        sample_rate,
+        center_frequency,
+        start_time_stamp,
+        sample_size,
+        filler,
+        crc32,
+    ) = struct.unpack("<IQQIII", header.tobytes())
+
+    if sample_size == 16:
+        config = {"dtype": np.int16, "scale": 0xFFFF}
+    elif sample_size == 24:
+        config = {"dtype": np.int32, "scale": 0xFFFFFF}
+    else:
+        raise Exception(f"Sample rate {sample_size} is not supported")
+
+    data = np.fromfile(
+        fid, dtype=config["dtype"], offset=32 + offset * 2, count=num_samples * 2
+    )
+    float_data = data.astype(np.float32) / config["scale"]
+    n = float_data[0::2] + 1j * float_data[1::2]
+
+    return n
+
+
 if __name__ == "__main__":
     import doctest
 
